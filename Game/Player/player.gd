938003d5c1b27e7ca43c80c624083cdebb50d472
DebjyotiBhowmick <debodoc001@gmail.com>
2023-12-31 18:31:37 +0100

    Simple changes

--- Game/Player/player.gd
+++ Game/Player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed: int= 40
 @onready var animations= $AnimationPlayer
 
+var currentHealth: int=3
+
 func handleInput():
 	
 	var moveDirection= Input.get_vector("ui_left","ui_right","ui_up","ui_down")
@@ -25,3 +27,8 @@ func _physics_process(delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
+
+
+func _on_hurt_box_area_entered(area):
+	if area.name == "hitBox":
+		print_debug(area.get_parent().name)
